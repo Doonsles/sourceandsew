c688ae4e4b1f1cda3907a83bf15be52303f111ef
Danielle Levinson <dclevinson@gmail.com>
2013-11-05 17:57:13 -0800

    added bootstrap styling

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,7 @@ module SourceAndSew
     config.assets.version = '1.0'
     
     config.assets.initialize_on_precompile = false
+    
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
