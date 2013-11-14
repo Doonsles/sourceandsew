ba91e6050c8607f0e05277dbb67b0a9c9f7ad94f
Danielle Levinson <dclevinson@gmail.com>
2013-11-14 10:56:00 -0800

    updated thumbnail appearance

--- app/controllers/factories_controller.rb
+++ app/controllers/factories_controller.rb
@@ -1,43 +1,6 @@
 class FactoriesController < ApplicationController
   before_filter :require_current_user!
   def index
-    #below works for non-pagination
-    # @factories = Factory.all
-    # @factories.each do |factory|
-    #   if Favorite.find_by_factory_id_and_user_id(factory.id, current_user.id)
-    #     factory.favorited = true
-    #   else
-    #     factory.favorited = false
-    #   end
-    # end
-    # 
-    # respond_to do |format|
-    #   format.html { render :index}
-    #   format.json { render :json => @factories }
-    # end
-    
-    
-    #below is example from AA GitHub page
-    # @factories = Factory.page(params[:page])
-    # @factories.each do |factory|
-    #   if Favorite.find_by_factory_id_and_user_id(factory.id, current_user.id)
-    #     factory.favorited = true
-    #   else
-    #     factory.favorited = false
-    #   end
-    # end
-    # 
-    # respond_to do |format|
-    #   format.html { render :index => @factories }
-    #   format.json {
-    #     render :json => { 
-    #       models: @factories,
-    #       page_number: params[:page],
-    #       total_pages: @factories.total_pages
-    #    }
-    #  }
-    # end
-    
     all_factories = Factory.all
     all_factories.each do |factory|
       if Favorite.find_by_factory_id_and_user_id(factory.id, current_user.id)
@@ -53,7 +16,6 @@ class FactoriesController < ApplicationController
       format.html { render :index}
       format.json { render :json => @factories }
     end 
-
   end
   
   def show
